176d2e00700386c8a9adb18d97c8aa97a47cf78b
ShimanchikIra <shimanchikirina@gmail.com>
2020-01-24 18:40:31 +0300

    Add method getStateAbbreviations

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let inputData = '"Nashville, TN", 36.17, -86.78;' +
+    '"New York, NY", 40.71, -74.00;' +
+    '"Atlanta, NY", 33.75, -84.39;' +
+    '"Denver, CO", 39.74, -104.98;' +
+    '"Seattle, WA", 47.61, -122.33;' +
+    '"Los Angeles, CA", 34.05, -118.24;' +
+    '"Memphis, TN", 35.15, -90.05;';
+
 function City(cityName, cityState, CityLatitudes, CityLongitudes) {
     this.name=cityName;
     this.state=cityState;
@@ -14,5 +22,14 @@ function CityMap(listOfCities) {
         });
         this.cities.push(new City(...cityFields));
     });
-
+    this.getStateAbbreviations = function () {
+        let result = [];
+        this.cities.forEach(city => {
+            if (!result.includes(city.state)) {
+                result.push(city.state);
+            }
+        });
+        return result.join(' ');
+    };
 }
+
